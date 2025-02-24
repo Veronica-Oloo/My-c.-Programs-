52585ce7cba2b62fd8c9bf207659cab4aa64097f
Veronica-Oloo <Veronicaoloo274@gmail.com>
2025-02-24 11:15:29 +0300

    Add files via upload

---
+++ Rectangle Area.C
@@ -0,0 +1,14 @@
+//program to find the area of rectangle
+
+#include<stdio.h>
+int main() {
+  int length=0;
+  int width=0;
+  printf("length:\n");
+  scanf("%d",&length);//input length
+  printf("width:\n");
+  scanf("%d",&width);//input width
+  int area= length *width;
+  printf("area:%d\n",area);// output the area based on the formula A=L*W 
+  return 0;
+}
