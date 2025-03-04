74454d9d1b32c18d4abf5da4f63cbc30d6b2868b
Veronica-Oloo <Veronicaoloo274@gmail.com>
2025-03-04 15:09:47 +0300

    Add files via upload

---
+++ calculation of books overdue .c
@@ -0,0 +1,49 @@
+/*
+//program to calculate fine for overview books
+Author: Veronica Oloo 
+Reg no:D33-2471-2023
+Version 1
+Date:29/2/2025
+*/
+#include<studio.h>//preprocessor 
+
+//main function
+int main()}
+//variable declaration
+int bookID;
+int duedate;
+int returndate;
+int daysoverdue;
+int finerate
+int fineamount
+//prompt the user to enter book ID
+printf("ENT or book ID:/n")
+Scanf("%d,",$ bookID);
+
+//prompt the user to enter dueDate
+Printf("enter the dueDate:)n");
+Scanf("%d",$dueDate);
+
+//prompt the user to enter return Date
+Printf("enter the return Date:/n")
+Scanf("%d,$ return Date)
+
+//prompt the user to calculate the number of days overdue
+days overdue= return Date-due Date;
+
+//determine the fineRate
+if(days overdue>=0&& daysOverdue<=7){
+printf("fine=daysoverdue*20");//ksh 20 per day
+}
+
+else if(daysoverdue>=8 && daysoverdue<=14){
+Printf("fine=daysoverdue*50);//ksh 50 per day
+}
+
+else(daysoverdue>=15);{
+printf("fine=daysoverdue*100");//ksh 100 per day
+}
+
+
+return 0; 
+}
